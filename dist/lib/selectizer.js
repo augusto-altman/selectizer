ac742c7528d20848a6cedc7a207f835560228f77
Augusto Altman Quaranta <augusto.altman@gmail.com>
2014-08-17 19:01:02 -0300

    refactoring of the GenerationHandler constructor. Some good practices aplied

--- dist/lib/selectizer.js
+++ dist/lib/selectizer.js
@@ -45,9 +45,8 @@ function ObjectSources(obj, objf) {
 
 	return objsrc;
 }
-function GenerationHandler(obj, objf, callback) {
-	var genhdlr = Object.create(ObjectSources(obj, objf)),
-		fs = require('fs'),
+var GenerationHandler = (function() {
+	var fs = require('fs'),
 		mkdirp = require('mkdirp'),
 		requirejs = require('requirejs');
 
@@ -56,51 +55,49 @@ function GenerationHandler(obj, objf, callback) {
 			count = 0,
 			initCodeFile = null;
 
-		for (var inc in genhdlr.data.config.modules) {
+		for (var inc in this.data.config.modules) {
 			if (count === 0) {
-				output += '"' + genhdlr.data.config.modules[inc] + '"';
+				output += '"' + this.data.config.modules[inc] + '"';
 			} else {
-				output += ', "' + genhdlr.data.config.modules[inc] + '"';
+				output += ', "' + this.data.config.modules[inc] + '"';
 			}
 			count++;
 		}
 
 		output += '], function() {\n';
 
-		initCodeFile = fs.readFileSync(genhdlr.data.paths.config + genhdlr.data.object + '.initCode.js', 'utf8');
+		initCodeFile = fs.readFileSync(this.data.paths.config + this.data.object + '.initCode.js', 'utf8');
 
 		output += initCodeFile + '\n' + '});';
 
-		return fs.writeFileSync(genhdlr.data.paths.build + genhdlr.data.object + '-init.js', output);
+		return fs.writeFileSync(this.data.paths.build + this.data.object + '-init.js', output);
 	}
 
-	if (!!genhdlr.success) {
-		genhdlr.data.requireJSOptions = {
-			baseUrl: genhdlr.data.paths.src,
-			name: "../build/" + genhdlr.data.object + "-init",
-			out: genhdlr.data.paths.build + genhdlr.data.object + ".js",
-			optimize: "none",
-			useStrict: true,
-			paths: genhdlr.data.config.paths,
-			wrap: genhdlr.data.config.wrap,
-			onBuildWrite: function(id, path, contents) {
-				if ((/define\(.*?\{/).test(contents)) {
-					contents = contents.replace(/define\(.*?\{/, '');
+	function generateBuild(callback) {
+		mkdirp(this.data.paths.build, (function(err) {
+			if (!!err) {
+				callback({
+					success: false,
+					data: {
+						errorMsg: err,
+						errorCode: 0
+					}
+				});
+			} else {
+				generateInit.call(this);
+				requirejs.optimize(this.data.requireJSOptions, (function() {
 
-					contents = contents.replace(/\}\);\s*?$/, '');
+					var mod = fs.readFileSync(this.data.requireJSOptions.out, 'utf8');
 
-					contents = contents.replace(/return.*[^return]*$/, '');
-				} else if ((/require\([^\{]*?\{/).test(contents)) {
-					contents = contents.replace(/require[^\{]+\{/, '');
-					contents = contents.replace(/\}\);\s*$/, '');
-				}
-				return contents;
-			}
-		};
+					mod = mod.replace('define("../build/' + this.data.object + '-init", function(){});', '');
+					mod = mod.replace('define("' + this.data.object + '", function(){});', '');
+					mod = mod.replace(/\s*?\n\s*?;\n/g, '\n');
 
-		genhdlr.generate = function() {
-			mkdirp(this.data.paths.build, (function(err) {
-				if (!!err) {
+					fs.writeFileSync(this.data.requireJSOptions.out, mod, 'utf8');
+
+					fs.unlinkSync(this.data.paths.build + this.data.object + '-init.js');
+				}).bind(this), function(err) {
+					fs.unlinkSync(this.data.paths.build + this.data.object + '-init.js');
 					callback({
 						success: false,
 						data: {
@@ -108,34 +105,42 @@ function GenerationHandler(obj, objf, callback) {
 							errorCode: 0
 						}
 					});
-				} else {
-					generateInit();
-					requirejs.optimize(this.data.requireJSOptions, (function() {
-
-						var mod = fs.readFileSync(this.data.requireJSOptions.out, 'utf8');
-
-						mod = mod.replace('define("../build/' + this.data.object + '-init", function(){});', '');
-						mod = mod.replace('define("' + this.data.object + '", function(){});', '');
-						mod = mod.replace(/\s*?\n\s*?;\n/g, '\n');
-
-						fs.writeFileSync(this.data.requireJSOptions.out, mod, 'utf8');
-
-						fs.unlinkSync(this.data.paths.build + this.data.object + '-init.js');
-					}).bind(this), function(err) {
-						fs.unlinkSync(this.data.paths.build + this.data.object + '-init.js');
-						callback({
-							success: false,
-							data: {
-								errorMsg: err,
-								errorCode: 0
-							}
-						});
-					});
-				}
-			}).bind(this));
-		};
+				});
+			}
+		}).bind(this));
 	}
 
-	callback(genhdlr);
-}
+
+	return function(obj, objf) {
+		var genhdlr = Object.create(ObjectSources(obj, objf));
+
+		if (!!genhdlr.success) {
+			genhdlr.generate = generateBuild;
+			genhdlr.data.requireJSOptions = {
+				baseUrl: genhdlr.data.paths.src,
+				name: "../build/" + genhdlr.data.object + "-init",
+				out: genhdlr.data.paths.build + genhdlr.data.object + ".js",
+				optimize: "none",
+				useStrict: true,
+				paths: genhdlr.data.config.paths,
+				wrap: genhdlr.data.config.wrap,
+				onBuildWrite: function(id, path, contents) {
+					if ((/define\(.*?\{/).test(contents)) {
+						contents = contents.replace(/define\(.*?\{/, '');
+
+						contents = contents.replace(/\}\);\s*?$/, '');
+
+						contents = contents.replace(/return.*[^return]*$/, '');
+					} else if ((/require\([^\{]*?\{/).test(contents)) {
+						contents = contents.replace(/require[^\{]+\{/, '');
+						contents = contents.replace(/\}\);\s*$/, '');
+					}
+					return contents;
+				}
+			};
+		}
+
+		return genhdlr;
+	};
+})();
 module.exports = GenerationHandler;
